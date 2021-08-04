e965447c0d36b039c9a743c138a997b815f15590
BitInByte <bitinbyte@outlook.com>
2021-08-04 16:57:38 +0100

    Section10 Completed

---
+++ spring-demo-annotations/src/com/bitinbyte/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,26 @@
+package com.bitinbyte.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * JavaConfigDemoApp
+ */
+public class JavaConfigDemoApp {
+    public static void main(String[] args) {
+        // Read Spring config file
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        // Get the bean from Spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        // Call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // Call method to get the daily fortune
+        System.out.println(theCoach.getDailyFortune());
+
+        // Close the context
+        context.close();
+
+    }
+}
