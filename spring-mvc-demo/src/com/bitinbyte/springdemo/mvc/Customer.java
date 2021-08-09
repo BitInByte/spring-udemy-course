e42db82a80a617744694f4670dbae0887a709919
BitInByte <bitinbyte@outlook.com>
2021-08-09 21:38:48 +0100

    Section17 Completed

--- spring-mvc-demo/src/com/bitinbyte/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/bitinbyte/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.bitinbyte.springdemo.mvc.validation.CourseCode;
+
 /**
  * Customer
  */
@@ -27,6 +29,9 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "PAZ", message = "must start with PAZ")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -58,5 +63,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
